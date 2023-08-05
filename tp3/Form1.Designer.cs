38f5d02756f85dca55f2fe856e9dd7b6bd618ea6
ariel <santa.cruz.unla@gmail.com>
2023-08-05 18:49:43 -0300

    Agregar archivos de proyecto.

---
+++ tp3/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace tp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.btn_add_1 = new System.Windows.Forms.Button();
+            this.btn_add_2 = new System.Windows.Forms.Button();
+            this.btn_sum = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(72, 57);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(124, 204);
+            this.listBox1.TabIndex = 0;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 20;
+            this.listBox2.Location = new System.Drawing.Point(270, 60);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(124, 204);
+            this.listBox2.TabIndex = 1;
+            // 
+            // listBox3
+            // 
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.ItemHeight = 20;
+            this.listBox3.Location = new System.Drawing.Point(484, 61);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(125, 204);
+            this.listBox3.TabIndex = 2;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(71, 310);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(124, 26);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(270, 310);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(124, 26);
+            this.textBox2.TabIndex = 4;
+            // 
+            // btn_add_1
+            // 
+            this.btn_add_1.Location = new System.Drawing.Point(70, 365);
+            this.btn_add_1.Name = "btn_add_1";
+            this.btn_add_1.Size = new System.Drawing.Size(125, 43);
+            this.btn_add_1.TabIndex = 6;
+            this.btn_add_1.Text = "agregar";
+            this.btn_add_1.UseVisualStyleBackColor = true;
+            this.btn_add_1.Click += new System.EventHandler(this.btn_add_1_Click);
+            // 
+            // btn_add_2
+            // 
+            this.btn_add_2.Location = new System.Drawing.Point(269, 365);
+            this.btn_add_2.Name = "btn_add_2";
+            this.btn_add_2.Size = new System.Drawing.Size(125, 43);
+            this.btn_add_2.TabIndex = 7;
+            this.btn_add_2.Text = "agregar";
+            this.btn_add_2.UseVisualStyleBackColor = true;
+            // 
+            // btn_sum
+            // 
+            this.btn_sum.Location = new System.Drawing.Point(484, 310);
+            this.btn_sum.Name = "btn_sum";
+            this.btn_sum.Size = new System.Drawing.Size(125, 43);
+            this.btn_sum.TabIndex = 8;
+            this.btn_sum.Text = "sumar filas";
+            this.btn_sum.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn_sum);
+            this.Controls.Add(this.btn_add_2);
+            this.Controls.Add(this.btn_add_1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listBox3);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.ListBox listBox3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button btn_add_1;
+        private System.Windows.Forms.Button btn_add_2;
+        private System.Windows.Forms.Button btn_sum;
+    }
+}
+
---
+++ tp3/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tp3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_add_1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "" && listBox1.Items.Count < 5)
+            {
+                listBox1.Items.Add(textBox1.Text);
+                textBox1.Clear();
+            }
+            else
+            {
+                MessageBox.Show("Error de Carga");
+            }
+        }
+    }
+}
